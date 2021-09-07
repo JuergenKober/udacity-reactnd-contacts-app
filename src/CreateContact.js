cf43827f3512b53e846d1433ef6e65ea1ef3a9ac
Jürgen Kober <mindeleven@posteo.de>
2021-09-07 15:56:31 +0200

    Integrated ImageInput component

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput';
 
 class CreateContact extends Component {
   render() {
@@ -9,6 +10,13 @@ class CreateContact extends Component {
           to='/'
           className='close-create-contact'
         >Close</Link>
+        <form className='create-contact-form'>
+          <ImageInput
+            className='create-contact-avatar-input'
+            name='avatarURL'
+            maxHeight={64}
+          />
+        </form>
       </div>
     )
   }
