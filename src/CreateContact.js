816f54a5bd1a9207de7ac1c32c2cb532526bba03
Jürgen Kober <mindeleven@posteo.de>
2021-09-07 15:51:37 +0200

    Integrate Link to navigate between screens

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class CreateContact extends Component {
   render() {
     return (
-      <div>CreateContact</div>
+      <div>
+        <Link
+          to='/'
+          className='close-create-contact'
+        >Close</Link>
+      </div>
     )
   }
 }
